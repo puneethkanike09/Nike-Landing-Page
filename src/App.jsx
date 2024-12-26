ae8378811e9ad3126d850735bef186bfc9d6d696
Puneeth K <reachout.puneeth@gmail.com>
2024-12-26 18:42:10 +0530

    Done with the boiler plate for the App.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 
 const App = () => {
   return (
-    <div className="text-4xl underline">
-      Hello there
-    </div>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        hero
+      </section>
+      <section className="padding">
+        Popular
+      </section>
+      <section className="padding">
+        Super quality
+      </section>
+      <section className="padding-x py-10">
+        Services
+      </section>
+      <section className="padding">
+        Special offer
+      </section>
+      <section className="padding bg-pale-blue padding">
+        Customer review
+      </section>
+      <section className="padding padding-x sm:py-32 py-16 w-full">
+        Subscribe
+      </section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
   )
 }
 
