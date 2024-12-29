03464b6f5bd1bd85f44755c65b979b96ae19c48d
Puneeth K <reachout.puneeth@gmail.com>
2024-12-29 10:13:50 +0530

    Completed the Subscribe and Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -13,34 +13,34 @@ import {
 
 const App = () => {
   return (
-    <main className="relative">
+    <main className='relative'>
       <Nav />
-      <section className="xl:padding-l wide:padding-r padding-x">
+      <section className='xl:padding-l wide:padding-r padding-x'>
         <Hero />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <Populer />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <Super />
       </section>
-      <section className="padding-x py-10">
+      <section className='padding-x py-10'>
         <Services />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <Offer />
       </section>
-      <section className="padding bg-pale-blue padding">
+      <section className='bg-pale-blue padding'>
         <Review />
       </section>
-      <section className="padding padding-x sm:py-32 py-16 w-full">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="padding bg-black padding-x padding-t pb-8">
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
     </main>
-  )
-}
+  );
+};
 
 export default App
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,30 +1,29 @@
-import { statistics } from "../constants";
-
-const Button = ({ label, iconURL }) => {
+const Button = ({
+    label,
+    iconURL,
+    backgroundColor,
+    textColor,
+    borderColor,
+    fullWidth,
+}) => {
     return (
-        <div>
-            <button
-                className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red transition-shadow duration-300 hover:shadow-[0_0_15px_3px_rgba(255,105,97,0.7)] group"
-            >
-                {label}
+        <button
+            className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${backgroundColor
+                    ? `${backgroundColor} ${textColor} ${borderColor}`
+                    : "bg-coral-red text-white border-coral-red"
+                } rounded-full ${fullWidth && "w-full"}`}
+        >
+            {label}
+
+            {iconURL && (
                 <img
                     src={iconURL}
-                    alt="icon"
-                    className="ml-2 rounded-full w-5 h-5 transition-transform duration-300 group-hover:rotate-180"
+                    alt='arrow right icon'
+                    className='ml-2 rounded-full bg-white w-5 h-5'
                 />
-            </button>
-
-
-
-            <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-                {statistics.map((item, index) => (
-                    <div key={index}>
-                        <p className="text-4xl font-palanquin font-bold">{item.value}</p>
-                        <p className="leading-7 font-montserrat text-slate-gray">{item.label}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
+            )}
+        </button>
     );
 };
 
