1427abd94817e48782778c3d500913a6adacbf91
Puneeth K <reachout.puneeth@gmail.com>
2024-12-29 17:17:31 +0530

    Added hover effects for the buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,23 +7,26 @@ const Button = ({
     fullWidth,
 }) => {
     return (
-        <button
-            className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
-      ${backgroundColor
-                    ? `${backgroundColor} ${textColor} ${borderColor}`
-                    : "bg-coral-red text-white border-coral-red"
-                } rounded-full ${fullWidth && "w-full"}`}
-        >
-            {label}
+        <div className="group">
+            <button
+                className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+                    ${backgroundColor
+                        ? `${backgroundColor} ${textColor} ${borderColor}`
+                        : "bg-coral-red text-white border-coral-red"
+                    } rounded-full ${fullWidth && "w-full"}
+                transition-all duration-300 ease-in-out `}
+            >
+                {label}
 
-            {iconURL && (
-                <img
-                    src={iconURL}
-                    alt='arrow right icon'
-                    className='ml-2 rounded-full bg-white w-5 h-5'
-                />
-            )}
-        </button>
+                {iconURL && (
+                    <img
+                        src={iconURL}
+                        alt="arrow right icon"
+                        className="ml-2 rounded-full bg-white w-5 h-5 transition-transform duration-300 ease-in-out transform group-hover:rotate-180" // Icon rotates on button hover
+                    />
+                )}
+            </button>
+        </div>
     );
 };
 
--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -3,7 +3,8 @@ import { star } from "../assets/icons";
 export const PopularProductCard = ({ imgURL, name, price }) => {
     return (
         <div className='flex flex-1 flex-col w-full max-sm:w-full'>
-            <img src={imgURL} alt={name} className='w-[282px] h-[282px]' />
+            <img src={imgURL} alt={name} className='w-[282px] h-[282px] hover:-translate-y-5 transition-all duration-300
+' />
             <div className='mt-8 flex justify-start gap-2.5'>
                 <img src={star} alt='rating icon' width={24} height={24} />
                 <p className='font-montserrat text-xl leading-normal text-slate-gray'>
